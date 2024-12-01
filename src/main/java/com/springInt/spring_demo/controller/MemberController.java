60696deae17130e491bdef79f6b95d18cd245ec8
SunJin_Yoon <sunjin.yoon@fnsusa.com>
2024-12-01 13:35:42 -0500

    Create Account function added
    Create Account & Inquiry Created Account.

--- src/main/java/com/springInt/spring_demo/controller/MemberController.java
+++ src/main/java/com/springInt/spring_demo/controller/MemberController.java
@@ -1,8 +1,14 @@
 package com.springInt.spring_demo.controller;
 
+import com.springInt.spring_demo.domain.Member;
 import com.springInt.spring_demo.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -12,5 +18,27 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm.html";
+    }
+
+    //You can change the content based on whether it's a GET("GetMapping") or POST("PostMapping") request.
+    @PostMapping("/members/new")
+    public String create(MemberForm memberForm) {
+        Member member = new Member();
+        member.setName(memberForm.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findAll();
+        model.addAttribute("members", members);
+        return "members/memberlist.html";
     }
 }
