3abcf0d9a1a128a6f6fe0df211ca6e30f7404adb
SunJin_Yoon <sunjin.yoon@fnsusa.com>
2024-12-01 22:49:43 -0500

    added DataBase - Mysql
    Need to fix networking to access local Database.

---
+++ sql/ddl.sql
@@ -0,0 +1,7 @@
+-- Change History Logs
+DROP TABLE IF EXISTS awsPrj94.tb_member_springDemo;
+CREATE TABLE awsPrj94.tb_member_springDemo (
+                                               id BIGINT AUTO_INCREMENT,
+                                               name VARCHAR(255),
+                                               PRIMARY KEY (id)
+);
--- src/main/java/com/springInt/spring_demo/SpringConfig.java
+++ src/main/java/com/springInt/spring_demo/SpringConfig.java
@@ -1,16 +1,39 @@
 package com.springInt.spring_demo;
 
+import com.springInt.spring_demo.repository.JdbcMemberRepository;
 import com.springInt.spring_demo.repository.MemberRepository;
 import com.springInt.spring_demo.repository.MemoryMemberRepository;
 import com.springInt.spring_demo.service.MemberService;
+import com.zaxxer.hikari.HikariDataSource;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 /**
  * Menually Add Bean and containers.
  */
 @Configuration
 public class SpringConfig {
+
+
+    @Bean
+    public DataSource dataSource() {
+        Dotenv dotenv = Dotenv.configure().load();
+        HikariDataSource hikariDataSource = new HikariDataSource();
+        hikariDataSource.setJdbcUrl(dotenv.get("MYSQL_URL"));
+        hikariDataSource.setUsername(dotenv.get("MYSQL_USERNAME"));
+        hikariDataSource.setPassword(dotenv.get("MYSQL_PASSWORD"));
+        return hikariDataSource;
+    }
+
+//    private DataSource dataSource;
+//    @Autowired
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
 
     @Bean
     public MemberService memberService() {
@@ -19,6 +42,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcMemberRepository(dataSource());
     }
 }
