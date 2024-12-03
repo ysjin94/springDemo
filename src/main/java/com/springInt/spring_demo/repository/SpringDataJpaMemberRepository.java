9090f618df3b93e9f08b7eeb6fbe48490fa9ce79
SunJin_Yoon <sunjin.yoon@fnsusa.com>
2024-12-03 16:37:44 -0500

    Added Spring JPA

---
+++ src/main/java/com/springInt/spring_demo/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,13 @@
+package com.springInt.spring_demo.repository;
+
+import com.springInt.spring_demo.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository
+{
+    //JPQL SELECT M FROM MEMBER M WHERE M.NAME = ?
+    @Override
+    Optional<Member> findByUsername(String username);
+}
